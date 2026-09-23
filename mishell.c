809057d9c4b7d0428e7969598e59e8f7f97d34ca
Emi4-4 <eallenrobles@gmail.com>
2026-09-22 22:18:06 -0300

    Implementacion de R3 y R4

--- mishell.c
+++ mishell.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 int main(void){
     char *line = NULL;
@@ -27,38 +28,119 @@ int main(void){
             break;
         }
 
-           char *args[64];
-           int i = 0;
-           char *token = strtok(line, " \t\n");
+        char *cmds[64];
+        int num_cmds = 0;
+        char *cmd_token = strtok(line, "|");
 
-           while (token != NULL && i < 63) {
-               args[i] = token;
-               i++;
-               token = strtok(NULL, " \n");
-           }
-           args[i] = NULL;
+        while (cmd_token != NULL && num_cmds < 64) {
+            cmds[num_cmds++] = cmd_token;
+            cmd_token = strtok(NULL, "|");
+        }
 
-        if (args[0] == NULL) {
+        if ( num_cmds== 0) {
             continue; 
         }
         
-        pid_t pid = fork();
+        int num_pipes = num_cmds - 1;
+        int pipefds[2 * num_pipes];
 
-        if (pid < 0) {
-            perror("fork() error");
-            exit(EXIT_FAILURE);
-
-         } else if (pid == 0) {
-             execvp(args[0], args);
-            perror("execvp() error");
-            exit(EXIT_FAILURE);
-        
-        } else {
-             waitpid(pid, NULL, 0);
+        for (int i = 0; i < num_pipes; i++) {
+            if (pipe(pipefds + i * 2) < 0) {
+                perror("pipe() error");
+                exit(EXIT_FAILURE);
+            }
         }
-    }
-    
-     free(line);
-    return 0;
 
+        pid_t pids[64];
+
+        for (int i = 0; i < num_cmds; i++) {
+            pids[i] = fork();
+
+            if (pids[i] < 0) {
+                perror("fork() error");
+                exit(EXIT_FAILURE);
+            } else if (pids[i] == 0) {
+
+                if (i > 0) {
+                    dup2(pipefds[(i - 1) * 2], STDIN_FILENO);
+                }
+                if (i < num_cmds - 1) {
+                    dup2(pipefds[i * 2 + 1], STDOUT_FILENO);
+                }
+
+                for (int j = 0; j < 2 * num_pipes; j++) {
+                    close(pipefds[j]);
+                }
+
+                char *args[64];
+                int arg_count = 0;
+                char *infile = NULL;
+                char *outfile = NULL;
+                int append = 0;
+
+                char *token = strtok(cmds[i], " \t\n");
+                while (token != NULL && arg_count < 63) {
+                    if (strcmp(token, "<") == 0) {
+                        infile = strtok(NULL, " \t\n");
+                    } else if (strcmp(token, ">") == 0) {
+                        outfile = strtok(NULL, " \t\n");
+                        append = 0; // Modo truncar
+                    } else if (strcmp(token, ">>") == 0) {
+                        outfile = strtok(NULL, " \t\n");
+                        append = 1; // Modo append
+                    } else {
+                        args[arg_count++] = token; 
+                    }
+                    token = strtok(NULL, " \t\n");
+                }
+
+                args[arg_count] = NULL;
+
+                if (args[0] == NULL) {
+                    exit(EXIT_SUCCESS); 
+                }
+
+                if (infile != NULL) {
+                    int fd_in = open(infile, O_RDONLY);
+                    if (fd_in < 0) {
+                        perror("open() error input");
+                        exit(EXIT_FAILURE);
+                    }
+                    dup2(fd_in, STDIN_FILENO);
+                    close(fd_in);
+                }
+
+                if (outfile != NULL) {
+                    int flags = O_WRONLY | O_CREAT;
+                    if (append) flags |= O_APPEND;
+                    else flags |= O_TRUNC;
+                    
+                    int fd_out = open(outfile, flags, 0644);
+                    if (fd_out < 0) {
+                        perror("open() error output");
+                        exit(EXIT_FAILURE);
+                    }
+                    dup2(fd_out, STDOUT_FILENO);
+                    close(fd_out);
+                }
+
+                execvp(args[0], args);
+                perror("execvp() error");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        for (int i = 0; i < 2 * num_pipes; i++) {
+            close(pipefds[i]);
+        }
+
+        
+        for (int i = 0; i < num_cmds; i++) {
+            waitpid(pids[i], NULL, 0);
+        }
+        
+    }
+    free(line);
+    return 0;
+   
 }
